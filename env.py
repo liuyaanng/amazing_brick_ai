8237d04ca0b8b54ffd4213606ce43fae84ffd3c3
liuyaanng <1757465118@qq.com>
2020-05-10 18:41:01 +0800

    something changes

--- env.py
+++ env.py
@@ -324,7 +324,7 @@ class AmazingBrickEnv():
     def __init__(self):
         """TODO: to be defined. """
         self.reset()
-        self.e = 0 # the currnet episode
+        self.e = 1 # the currnet episode
 
         
     def step(self, action):
@@ -339,7 +339,6 @@ class AmazingBrickEnv():
         action = random.randint(0,2)
         # action = 1
         self.player.action(action)
-        self.observation, reward , is_game_running= self.player.update()
         # print(self.observation, reward)
         
         print("score:", score)
@@ -390,8 +389,60 @@ class AmazingBrickEnv():
         
         if SHOW_PREVIEW:
             self.render()
-        image = arcade.get_image(0, 0, width = int(SCREEN_WIDTH), height = int(SCREEN_HIGHT) )
+        # image = arcade.get_image(0, 0, width = int(SCREEN_WIDTH), height = int(SCREEN_HIGHT) )
         # image.save('test.png')
+
+        agent.tensorboard.step = self.e
+
+        episode_reward = 0
+        step = 1
+        current_state = env.reset()
+        print(current_state)
+        is_game_running = True
+
+
+        while is_game_running:
+            if np.random.random() > epsilon:
+                action = np.argmax(agent.get_qs(current_state))
+            else:
+                action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+            new_state, reward, is_game_running = self.player.update
+            print(new_state)
+
+            episode_reward += reward
+            print(episode_reward)
+            if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                env.render()
+
+            agent.update_replay_memory((current_state, action, reward, new_state, is_game_running))
+
+            agent.train(is_game_running, step)
+
+            current_state = new_state
+
+            step += 1
+            self.e += 1
+
+
+            # Append episode reward to a list and log stats (every given number of episodes)
+        ep_rewards.append(episode_reward)
+        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+            # Save model, but only when min reward is greater or equal a set value
+            if min_reward >= MIN_REWARD:
+                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+        # Decay epsilon
+        if epsilon > MIN_EPSILON:
+            epsilon *= EPSILON_DECAY
+            epsilon = max(MIN_EPSILON, epsilon)
+
+
+
         return self.observation, reward, is_game_running
     def render(self):
         arcade.start_render()
